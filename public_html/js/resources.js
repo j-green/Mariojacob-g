3f687552b513b11a4ee11ce3ee295b3f5e9c8549
Jacob Green <jacobcgreen@verizon.net>
2015-01-13 14:03:30 -0800

    yea

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -1,4 +1,4 @@
-game.resources = [
+kkgame.resources = [
 
 	/* Graphics. 
 	 * @example
@@ -10,12 +10,14 @@ game.resources = [
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
 	 */
 		
+                {name: "background-tiles", type: "tps", src: "data/img/background-tiles.json"},
 	/* Maps. 
 	 * @example
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
 
+               {name: "level01", type: "tmx", src: "data/map/level01.tmx"}
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
